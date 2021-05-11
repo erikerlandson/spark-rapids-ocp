31f2aee10c220a015933f956a5ecc57eaef8f853
Erik Erlandson <eerlands@redhat.com>
2021-05-11 13:14:31 -0700

    version of spark shell w/out gpu

---
+++ scripts/run-spark-shell-nogpu.sh
@@ -0,0 +1,27 @@
+# These settings are for running spark shell from a pod running inside the cluster,
+# for example a pod created from:
+# oc run -i -t --serviceaccount=spark spark --image=quay.io/erikerlandson/spark-rapids:latest --command -- /bin/bash
+
+# Spark will want this Service created for Executors to route back to
+# the Driver running on this pod.
+
+# kind: Service
+# apiVersion: v1
+# metadata:
+#   name: < this pod name here >
+# spec:
+#   selector:
+#     deploymentconfig: spark
+#   clusterIP: None
+#   type: ClusterIP
+#   sessionAffinity: None
+
+${SPARK_HOME}/bin/spark-shell --master k8s://https://kubernetes.default:443 \
+             --conf spark.kubernetes.authenticate.submission.oauthToken=$(cat /run/secrets/kubernetes.io/serviceaccount/token) \
+             --conf spark.kubernetes.container.image=quay.io/erikerlandson/spark-rapids:latest \
+             --conf spark.driver.host=$(hostname) \
+             --conf spark.locality.wait=0s \
+             --conf spark.driver.memory=2g  --conf spark.executor.memory=4g \
+             --conf spark.executor.cores=1  --conf spark.task.cpus=1 \
+             --conf spark.sql.files.maxPartitionBytes=512m   \
+             --conf spark.sql.shuffle.partitions=10
